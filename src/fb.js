c2a11cceb0130320fab17498930f24182b0dd831
davidyoon <qqpo12@naver.com>
2022-05-30 21:15:00 +0900

    apply ngrok and faceBook Login

--- src/fb.js
+++ src/fb.js
@@ -45,24 +45,3 @@ module.exports = {
   getUserIdWithFacebookId,
   getUserAccessTokenForFacebookAccessToken,
 }
-
-window.fbAsyncInit = () => {
-  FB.init({
-    appId: FB_APP_ID,
-    cookie: true,
-    xfbml: true,
-    version: 'v10.0',
-  })
-
-  FB.AppEvents.logPageView()
-}
-;((d, s, id) => {
-  const fjs = d.getElementsByTagName(s)[0]
-  if (d.getElementById(id)) {
-    return
-  }
-  const js = d.createElement(s)
-  js.id = id
-  js.src = 'https://connect.facebook.net/en_US/sdk.js'
-  fjs.parentNode.insertBefore(js, fjs)
-})(document, 'script', 'facebook-jssdk')
--- src/public/fb.js
+++ src/public/fb.js
@@ -1,5 +1,27 @@
 window.fbAsyncInit = () => {
-  /* TODO: implement */
+  FB.init({
+    appId: APP_CONFIG.FB_APP_ID,
+    version: 'v14.0',
+  })
+
+  document.getElementById('fb-login').addEventListener('click', () => {
+    FB.login(
+      (response) => {
+        console.log(response)
+      },
+      { scope: 'public_profile,email' }
+    )
+  })
 }
+;((d, s, id) => {
+  const fjs = d.getElementsByTagName(s)[0]
+  if (d.getElementById(id)) {
+    return
+  }
+  const js = d.createElement(s)
+  js.id = id
+  js.src = 'https://connect.facebook.net/en_US/sdk.js'
+  fjs.parentNode.insertBefore(js, fjs)
+})(document, 'script', 'facebook-jssdk')
 
 // TODO: initialize Facebook SDK
